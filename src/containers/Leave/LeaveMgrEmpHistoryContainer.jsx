c7c36e73f641ea77a4694ce992ae1b2bf98bac5d
Ash Zhang <ash.norseman@gmail.com>
2016-04-12 14:24:11 +0800

    leave approve detail

--- src/containers/Leave/LeaveMgrEmpHistoryContainer.jsx
+++ src/containers/Leave/LeaveMgrEmpHistoryContainer.jsx
@@ -4,13 +4,14 @@
 
 
 import React, { Component } from 'react';
-import dispatcher, { dispatch } from '../../dispatcher/Dispatcher';
 import { Container } from 'flux/utils';
 
 import { getItem as getLang } from '../../common/lang';
+
 import Header from '../../components/Header/Header.jsx';
-import PullLoader from '../../components/PullLoader/PullLoader.jsx';
+import Loader from '../../components/Loader/Loader.jsx';
 import RecordList from '../../components/RecordList/RecordList.jsx';
+
 import LeaveStore from '../../stores/LeaveStore';
 import LeaveDataUtils from '../../data-utils/LeaveDataUtils';
 
@@ -28,40 +29,25 @@ class LeaveListContainer extends Component {
   constructor(props) {
     super(props);
 
-    this.getEmpLeaveRecords();
-  }
-
-  render() {
-    const { leaveRecord = {} } = this.state;
-
-    console.log(leaveRecord);
-
-    return (
-      <div>
-        <Header back title={getLang('LEAVE_SUMMARY')} />
-        <RecordList recordList={leaveRecord}
-                    url='leave-record-mgr' />
-      </div>
-    );
-  }
-
-
-  /**
-   * Get employees' leave records (parameters)
-   * @param params
-   */
-  getEmpLeaveRecords() {
     LeaveDataUtils.getApproveRecord(this.props.routeParams.id);
   }
 
+  render() {
+    const {
+      leaveRecord = [],
+      status = 'loading'
+    }  = this.state;
 
-  /**
-   * Update filter
-   * @param type
-   * @param choice
-   */
-  filter(type, choice) {
-    this.getEmpLeaveRecords({ type, choice });
+    return (
+      <div>
+        <Header goBack title={getLang('LEAVE_SUMMARY')} />
+
+        <Loader className='side-gap'
+                status={status}>
+          <RecordList recordList={leaveRecord} />
+        </Loader>
+      </div>
+    );
   }
 }
 
